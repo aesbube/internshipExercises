fb97097f1806f167cb7deeea264327ac060c1461
aesbube <ljubicad11@gmail.com>
2025-03-20 15:51:33 +0100

    Add api for heroes

---
+++ heroes-api/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import json
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/api/heroes")
+def get_heroes():
+    with open("heroes.json", "r") as file:
+        heroes = json.load(file)
+    return heroes
