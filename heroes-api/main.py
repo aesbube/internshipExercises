b1abcebb4a45db77bacdaa1085e71b0ef863e192
aesbube <ljubicad11@gmail.com>
2025-03-20 16:36:40 +0100

    Fixed api

--- heroes-api/main.py
+++ heroes-api/main.py
@@ -16,5 +16,15 @@ app.add_middleware(
 @app.get("/api/heroes")
 def get_heroes():
     with open("heroes.json", "r") as file:
-        heroes = json.load(file)
-    return heroes
+        data = json.load(file)
+    return data["heroes"]
+
+
+@app.get("/api/hero-detail/{id}")
+def get_heroes_id(id: int):
+    with open("heroes.json", "r") as file:
+        data = json.load(file)
+    heroes = data["heroes"]
+    hero = next((hero for hero in heroes if hero["id"] == id), None)
+    return hero
+
